0ec17352657dbf796b5a1217b7daa6e094a6339b
rhit-hurtigna <hurtigna@rose-hulman.edu>
2023-03-20 10:47:34 -0400

    plotting death SIR

--- SIR/death_SIR_test.m
+++ SIR/death_SIR_test.m
@@ -1,15 +1,17 @@
-function reinfect_SIR_test()
-%reinfect_SIR_test Graphs our SIR model with an example
+function death_SIR_test()
+%death_SIR_test Graphs our death SIR model with an example
 
-alpha = 0.08;
-beta = 0.8;
-kappa = 0.3;
+alpha = .8;
+beta = .1;
+rho = .3;
+delta = .1;
+epsilon = .1;
 S0 = 60;
 I0 = 2;
 R0 = 38;
-T = 10;
+T = 30;
 
-[time_domain,S,I,R] = reinfect_SIR(alpha,beta,0,S0,I0,R0,T);
+[time_domain,S,I,R] = death_SIR(alpha,beta,0,0,0,S0,I0,R0,T);
 
 figure;
 plot(time_domain,S,'Color','Green');
@@ -23,7 +25,7 @@ title("Traditional SIR model");
 
 hold off;
 
-[time_domain,S,I,R] = reinfect_SIR(alpha,beta,kappa,S0,I0,R0,T);
+[time_domain,S,I,R] = death_SIR(alpha,beta,rho,delta,epsilon,S0,I0,R0,T);
 
 figure;
 plot(time_domain,S,'Color','Green');
@@ -33,7 +35,7 @@ plot(time_domain,R,'Color','Red');
 ylabel("Population");
 xlabel("Time (t)");
 legend("Susceptible","Infected","Recovered")
-title("Adapted SIR model");
+title("Death SIR model");
 
 
 end
