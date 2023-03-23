ef21bdb68c20f8063fa8db32347ae1c909839422
rhit-hurtigna <hurtigna@rose-hulman.edu>
2023-03-23 14:41:45 -0400

    proof of sinusoidal waste pattern

--- cloud_computing/cloud_pretty.m
+++ cloud_computing/cloud_pretty.m
@@ -4,13 +4,14 @@ function cloud_pretty(g,rho,u,connected,kappa,V0,T,tstep)
 %see inputs of cloud_main
 
 N = size(rho,1);
-legend_names = strings(2*N+2,1);
+legend_names = strings(2*N+3,1);
 for num=1:N
     legend_names(num) = sprintf('queue_%d',num);
 end
 for num=1:N
     legend_names(num+N) = sprintf('proc_%d',num);
 end
+legend_names(end-2) = "packets in transit";
 legend_names(end-1) = "packets accounted for";
 legend_names(end) = "packets processed";
 
@@ -20,7 +21,9 @@ plot(time_domain,V(1:N,:));
 hold on;
 set(gca,'ColorOrderIndex',1);
 plot(time_domain,V(N+1:2*N,:),'LineStyle','--');
-plot(time_domain, sum(V),'LineStyle',':','Color','Black');
+sumV = sum(V);
+plot(time_domain, sum(V0)-sumV,'LineStyle',':','Color','Red');
+plot(time_domain, sumV,'LineStyle',':','Color','Black');
 plot(time_domain, sum(V(N+1:2*N,:)),'LineStyle',':','Color','Magenta');
 legend(legend_names);
 
--- cloud_computing/cloud_test.m
+++ cloud_computing/cloud_test.m
@@ -3,16 +3,28 @@ function cloud_test()
 
 gbasic = @(x1,x2,~,~,~) max(0,x1-x2);
 
-% Three processors, connected 1 - 2 - 3.
-rho = [1;1;1];
-u = [0 3 0 ; 0 1 0 ; 0 3 0];
-connected = [0 1 0 ; 1 0 1 ; 0 1 0];
-kappa = 2;
-V0 = [30;0;0];
-T = 80;
+% Two processors, connected 1 - 2. Processing speed is slow enough to make
+% the inefficiency obvious.
+rho = [1;1];
+u = [0 3 ; 3 0];
+connected = [0 1 ; 1 0];
+kappa = 20;
+V0 = [30;0];
+T = 350;
 tstep = 1e-2;
 
 cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
+
+% % Three processors, connected 1 - 2 - 3.
+% rho = [1;1;1];
+% u = [0 3 0 ; 0 1 0 ; 0 3 0];
+% connected = [0 1 0 ; 1 0 1 ; 0 1 0];
+% kappa = 2;
+% V0 = [30;0;0];
+% T = 80;
+% tstep = 1e-2;
+% 
+% cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
 
 end
 
