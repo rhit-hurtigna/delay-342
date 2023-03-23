c61ae7a4b9c8a7b21512a47e6979b68fa349474b
rhit-hurtigna <hurtigna@rose-hulman.edu>
2023-03-23 15:33:07 -0400

    make the graphs really pretty

--- cloud_computing/cloud_pretty.m
+++ cloud_computing/cloud_pretty.m
@@ -4,28 +4,30 @@ function cloud_pretty(g,rho,u,connected,kappa,V0,T,tstep)
 %see inputs of cloud_main
 
 N = size(rho,1);
-legend_names = strings(2*N+3,1);
+legend_names = strings(2*N+2,1);
 for num=1:N
     legend_names(num) = sprintf('queue_%d',num);
 end
 for num=1:N
     legend_names(num+N) = sprintf('proc_%d',num);
 end
-legend_names(end-2) = "packets in transit";
-legend_names(end-1) = "packets accounted for";
+legend_names(end-1) = "packets in transit";
 legend_names(end) = "packets processed";
 
 [time_domain,V] = cloud_main(g,rho,u,connected,kappa,V0,T,tstep);
 
-plot(time_domain,V(1:N,:));
+figure();
 hold on;
+plot(time_domain,V(1:N,:));
 set(gca,'ColorOrderIndex',1);
 plot(time_domain,V(N+1:2*N,:),'LineStyle','--');
 sumV = sum(V);
-plot(time_domain, sum(V0)-sumV,'LineStyle',':','Color','Red');
-plot(time_domain, sumV,'LineStyle',':','Color','Black');
-plot(time_domain, sum(V(N+1:2*N,:)),'LineStyle',':','Color','Magenta');
-legend(legend_names);
+plot(time_domain, sum(V0)-sumV,'LineStyle',':','Color','Black', ...
+    'LineWidth',1.5);
+plot(time_domain, sum(V(N+1:2*N,:)),'LineStyle',':','Color','Magenta', ...
+    'LineWidth',1.5);
+legend(legend_names,'Location','north');
+hold off;
 
 end
 
--- cloud_computing/cloud_test.m
+++ cloud_computing/cloud_test.m
@@ -8,23 +8,45 @@ gbasic = @(x1,x2,~,~,~) max(0,x1-x2);
 rho = [1;1];
 u = [0 3 ; 3 0];
 connected = [0 1 ; 1 0];
-kappa = 20;
-V0 = [30;0];
-T = 350;
-tstep = 1e-2;
+kappa = 50;
+V0 = [6;0];
+T = 210;
+tstep = 5e-3;
 
 cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
 
-% % Three processors, connected 1 - 2 - 3.
-% rho = [1;1;1];
-% u = [0 3 0 ; 0 1 0 ; 0 3 0];
-% connected = [0 1 0 ; 1 0 1 ; 0 1 0];
-% kappa = 2;
-% V0 = [30;0;0];
-% T = 80;
-% tstep = 1e-2;
-% 
-% cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
+% Two processors, connected 1 - 2. Really bad efficiency.
+rho = [1;1];
+u = [0 12 ; 12 0];
+connected = [0 1 ; 1 0];
+kappa = 3;
+V0 = [20;0];
+T = 180;
+tstep = 5e-3;
+
+cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
+
+% Two processors, connected 1 - 2. Same as above, but no delay.
+rho = [1;1];
+u = [0 0 ; 0 0];
+connected = [0 1 ; 1 0];
+kappa = 3;
+V0 = [20;0];
+T = 180;
+tstep = 5e-3;
+
+cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
+
+% Three processors, connected 1 - 2 - 3.
+rho = [1;1;1];
+u = [0 3 0 ; 0 1 0 ; 0 3 0];
+connected = [0 1 0 ; 1 0 1 ; 0 1 0];
+kappa = 2;
+V0 = [30;0;0];
+T = 45;
+tstep = 1e-2;
+
+cloud_pretty(gbasic,rho,u,connected,kappa,V0,T,tstep);
 
 end
 
