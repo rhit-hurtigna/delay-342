a2fc140057b0cbd7a3f30af8c405fbe74bb7b6f8
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-03-22 20:36:42 -0400

    Started playing around with history, got plots working

--- Circuit/Exercise_4_Solver.m
+++ Circuit/Exercise_4_Solver.m
@@ -4,21 +4,28 @@ clear;
 clc;
 
 % Find delays
-Ny      = 20;
-delyf   = -3*pi;
-Nyp     = 20;
-delypf  = -2*pi;
-dely    = linspace(delyf, 0, Ny);
-delyp   = linspace(delypf, 0, Nyp);
+Ny      = 2;
+delyf   = pi;
+Nyp     = 10;
+delypf  = 2*pi;
+dely    = linspace(0, delyf, Ny+1);
+delyp   = linspace(0, delypf, Nyp+1);
+dely    = dely(2:end);
+delyp   = delyp(2:end);
+
+hist    = history_circ(-dely);
 
 % Start and End Points
 t0  = 0;
 tf  = 5;
 
 % Solve DDE using ddensd
-sol = ddesnd(@ddefun_circ, dely, delyp, @history_circ, [t0  tf]);
+sol = ddensd(@ddefun_circ, dely, delyp, @history_circ, [t0  tf]);
+
+plotX   = [-dely, sol.x];
+plotY   = [hist, sol.y];
 
 % Plotting
 figure(1);
 clf;
-plot(sol.x, sol.y);
+plot(plotX, plotY);
