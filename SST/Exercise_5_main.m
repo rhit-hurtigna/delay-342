29496c557a6142f39bf8bb834da155a61b9e48ff
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-03-28 22:58:56 -0400

    Started to Optimize Exercise 5 Model

--- SST/Exercise_5_main.m
+++ SST/Exercise_5_main.m
@@ -9,22 +9,16 @@ alpha0  = 3;
 tau0    = 7;
 x0      = [alpha0; tau0];
 
-% Finding history
-hist_start  = 1940;
-hist_end    = hist_start + 10;
-fut_end     = hist_end + 10;
-hist_range  = [hist_start, hist_end];
-
-% Inequality conditions
-A       = -eye(length(x0));
-b       = [-0.3; -5];
+% Finding history                                                                   
+curr_yr    = 1950;
+fut_end     = curr_yr + 10;
 
 % Optimizing
-optim   = 0;
+optim   = 1;
 if (optim)
-    x_optim     = fmincon(@(x)func_ex5_resid(x, hist_range), x0, A, b);
-    alpha_opt   = x_optim(1);
-    tau_opt     = x_optim(2);
+    alpha_rng   = [1; 10];
+    tau_rng     = [5; 20];
+    [alpha_opt, tau_opt]    = func_grid_ex5(alpha_rng, tau_rng, curr_yr);
 else
     alpha_opt   = alpha0;
     tau_opt     = tau0;
@@ -32,7 +26,8 @@ end
 
 
 % Finding Optimized Model
-[t_hist, T_hist]    = func_ex3_history_pts(hist_start, hist_end, hist_end);
+hist_start = curr_yr - tau_opt;
+[t_hist, T_hist]    = func_ex3_history_pts(hist_start, curr_yr, curr_yr);
 hist_func   = @(v)CubicSpline(t_hist, T_hist, v);
 options = ddeset('RelTol', 1e-6, 'AbsTol', 1e-6);
 sol     = dde23(@(t, y, ydel)ddefun_SST3(t, y, ydel, alpha_opt), tau_opt, hist_func, [0, 10], options);
@@ -40,7 +35,7 @@ t_optim  = (sol.x)';
 T_optim  = (sol.y)';
 
 % Finding history
-[t_fut, T_fut]      = func_ex3_history_pts(hist_end, fut_end, hist_end);
+[t_fut, T_fut]      = func_ex3_history_pts(curr_yr, fut_end, curr_yr);
 
 % Plotting and Analysis
 figure(1);
@@ -49,7 +44,7 @@ plot(t_optim, T_optim, 'b', t_fut, T_fut, 'k+');
 xlabel("Time (t) - Yearly Index");
 ylabel("Temperature");
 legend("Optimized DDE Model", "Measured History");
-title("Temperature over " + hist_end + " to " + fut_end);
+title("Temperature over " + curr_yr + " to " + fut_end);
 
 disp("Optimal residual value found at alpha = " + alpha_opt);
 disp("Optimal residual value found at tau = " + tau_opt);
--- SST/func_grid_ex5.m
+++ SST/func_grid_ex5.m
@@ -1,14 +1,75 @@
-function [alpha_optim, tau_optim] = func_grid_ex5(alpha_rng, tau_rng)
+function [alpha_optim, tau_optim] = func_grid_ex5(alpha_rng, tau_rng, curr_yr)
     % Function optimizes the ex5 function based on a grid search.
     % INPUTS
     % alpha_rng -- 2x1 vector representing desired range of alpha values
     %              the function will optimize over. 
     % tau_rng -- 2x1 vector representing desired range of tau values the 
     %            function will optimize over.
+    % hist_rng -- 1x2 vector representing the history range
     % OUTPUTS
     % alpha_optim -- alpha value which gave lowest residual
     % tau_optim -- tau value which gave lowest residual
     
-    
+    % Split apart inputs
+    alpha_min   = alpha_rng(1);
+    alpha_max   = alpha_rng(2);
+    tau_min     = tau_rng(1);
+    tau_max     = tau_rng(2);
+
+    % Define number of points for alpha and tau
+    alpha_pts = 25;
+    tau_pts = 25;
+    num_optim   = 2;
+
+    % Optimize
+    while (num_optim > 0)
+        R_min   = intmax;
+        alpha_optim = 0;
+        tau_optim = 0;
+        for alph = linspace(alpha_min, alpha_max, alpha_pts)
+            for tau = linspace(tau_min, tau_max, tau_pts)
+                x_curr      = [alph; tau];
+                hist_rng    = [curr_yr-tau; curr_yr];       
+                R_curr  = func_ex5_resid(x_curr, hist_rng);
+                if (R_curr < R_min) 
+                    alpha_optim = alph;
+                    tau_optim = tau;
+                    R_min   = R_curr;
+                end
+            end
+        end
+
+        % Setting up for next iteration
+        if (num_optim ~= 1)
+            del_alpha = (alpha_max-alpha_min)/(alpha_pts-1);
+            del_tau = (tau_max-tau_min)/(tau_pts-1);
+            disp("Changing alpha by " + del_alpha);
+            disp("Changing tau by " + del_tau);
+            alpha_max1   = alpha_optim + del_alpha;
+            alpha_min1   = alpha_optim - del_alpha;
+            tau_max1     = tau_optim + del_tau;
+            tau_min1     = tau_optim - del_tau;
+
+            % Check edge cases
+            if (alpha_max1 > alpha_max)
+                alpha_min   = alpha_min1 - (alpha_max1 - alpha_max);
+            elseif (alpha_min1 < alpha_min)
+                alpha_max   = alpha_max1 + (alpha_min - alpha_min1);
+            else
+                alpha_min   = alpha_min1;
+                alpha_max   = alpha_max1;
+            end
+            if (tau_max1 > tau_max)
+                tau_min   = tau_min1 - (tau_max1 - tau_max);
+            elseif (tau_min1 < tau_min)
+                tau_max   = tau_max1 + (tau_min - tau_min1);
+            else
+                tau_min   = tau_min1;
+                tau_max   = tau_max1;
+            end
+        end
+
+        num_optim = num_optim - 1;
+    end
 
 end
