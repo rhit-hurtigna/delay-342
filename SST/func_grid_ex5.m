0bec2fa2c7e176ea5f5112025b259658836d777d
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-03-30 23:27:29 -0400

    More experimenting

--- SST/func_grid_ex5.m
+++ SST/func_grid_ex5.m
@@ -19,55 +19,55 @@ function [alpha_optim, tau_optim] = func_grid_ex5(alpha_rng, tau_rng, curr_yr)
     % Define number of points for alpha and tau
     alpha_pts = 25;
     tau_pts = 25;
-    num_optim   = 2;
+    num_optim   = 1;
 
     % Optimize
     while (num_optim > 0)
-        R_min   = intmax;
-        alpha_optim = 0;
-        tau_optim = 0;
+        R_min   = 10000000000*ones(3,1);
+        alpha_optim = zeros(3,1);
+        tau_optim = zeros(3,1);
         for alph = linspace(alpha_min, alpha_max, alpha_pts)
             for tau = linspace(tau_min, tau_max, tau_pts)
                 x_curr      = [alph; tau];
                 hist_rng    = [curr_yr-tau; curr_yr];       
                 R_curr  = func_ex5_resid(x_curr, hist_rng);
-                if (R_curr < R_min) 
-                    alpha_optim = alph;
-                    tau_optim = tau;
-                    R_min   = R_curr;
-                end
+                alph_curr   = alph*ones(3,1);
+                tau_curr    = tau*ones(3,1);
+                alpha_optim(R_curr < R_min)     = alph_curr(R_curr < R_min);
+                tau_optim(R_curr < R_min)       = tau_curr(R_curr < R_min);
+                R_min(R_curr < R_min)           = R_curr(R_curr < R_min);
             end
         end
 
-        % Setting up for next iteration
-        if (num_optim ~= 1)
-            del_alpha = (alpha_max-alpha_min)/(alpha_pts-1);
-            del_tau = (tau_max-tau_min)/(tau_pts-1);
-            disp("Changing alpha by " + del_alpha);
-            disp("Changing tau by " + del_tau);
-            alpha_max1   = alpha_optim + del_alpha;
-            alpha_min1   = alpha_optim - del_alpha;
-            tau_max1     = tau_optim + del_tau;
-            tau_min1     = tau_optim - del_tau;
-
-            % Check edge cases
-            if (alpha_max1 > alpha_max)
-                alpha_min   = alpha_min1 - (alpha_max1 - alpha_max);
-            elseif (alpha_min1 < alpha_min)
-                alpha_max   = alpha_max1 + (alpha_min - alpha_min1);
-            else
-                alpha_min   = alpha_min1;
-                alpha_max   = alpha_max1;
-            end
-            if (tau_max1 > tau_max)
-                tau_min   = tau_min1 - (tau_max1 - tau_max);
-            elseif (tau_min1 < tau_min)
-                tau_max   = tau_max1 + (tau_min - tau_min1);
-            else
-                tau_min   = tau_min1;
-                tau_max   = tau_max1;
-            end
-        end
+%         % Setting up for next iteration
+%         if (num_optim ~= 1)
+%             del_alpha = (alpha_max-alpha_min)/(alpha_pts-1);
+%             del_tau = (tau_max-tau_min)/(tau_pts-1);
+%             disp("Changing alpha by " + del_alpha);
+%             disp("Changing tau by " + del_tau);
+%             alpha_max1   = alpha_optim + del_alpha;
+%             alpha_min1   = alpha_optim - del_alpha;
+%             tau_max1     = tau_optim + del_tau;
+%             tau_min1     = tau_optim - del_tau;
+% 
+%             % Check edge cases
+%             if (alpha_max1 > alpha_max)
+%                 alpha_min   = alpha_min1 - (alpha_max1 - alpha_max);
+%             elseif (alpha_min1 < alpha_min)
+%                 alpha_max   = alpha_max1 + (alpha_min - alpha_min1);
+%             else
+%                 alpha_min   = alpha_min1;
+%                 alpha_max   = alpha_max1;
+%             end
+%             if (tau_max1 > tau_max)
+%                 tau_min   = tau_min1 - (tau_max1 - tau_max);
+%             elseif (tau_min1 < tau_min)
+%                 tau_max   = tau_max1 + (tau_min - tau_min1);
+%             else
+%                 tau_min   = tau_min1;
+%                 tau_max   = tau_max1;
+%             end
+%         end
 
         num_optim = num_optim - 1;
     end
