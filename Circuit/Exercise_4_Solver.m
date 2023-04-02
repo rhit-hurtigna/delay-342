406fa4cda1b90187fe5045eaf18fa2407824dc96
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-04-02 15:13:36 -0400

    More experimenting with tau

--- Circuit/Exercise_4_Solver.m
+++ Circuit/Exercise_4_Solver.m
@@ -4,37 +4,36 @@ clear;
 clc;
 
 % Find delays, which are linearly spaced constants. 
-for delay = 50:50:200
-    Ny      = 1;
-    delyf   = delay;
-    Nyp     = 1;
-    delypf  = delay;
-    dely    = linspace(0, delyf, Ny+1);
-    delyp   = linspace(0, delypf, Nyp+1);
-    
-    dely    = dely(2:end);
-    delyp   = delyp(2:end);
-    
-    y_hist_need = linspace(-delyf, 0, 100);
-    
-    hist    = history_circ(y_hist_need);
-    
-    % Start and End Points
-    t0  = 0;
-    tf  = 10;
-    
-    % Solve DDE using ddensd
-    sol = ddensd(@ddefun_circ, dely, delyp, @history_circ, [t0  tf]);
-    
-    plotX   = [y_hist_need, sol.x];
-    plotY   = [hist, sol.y];
-    
-    % Plotting
-    figure(delay/50);
-    clf;
-    plot(plotX, plotY);
-    xlabel("Time t");
-    ylabel("y(t)");
-    legend("y_1(t)", "y_2(t)", "y_3(t)");
-    title("Tau = " + delay);
-end
+delay   = 18;
+Ny      = 1;
+delyf   = delay;
+Nyp     = 1;
+delypf  = delay;
+dely    = linspace(0, delyf, Ny+1);
+delyp   = linspace(0, delypf, Nyp+1);
+
+dely    = dely(2:end);
+delyp   = delyp(2:end);
+
+y_hist_need = linspace(-delyf, 0, 100);
+
+hist    = history_circ(y_hist_need);
+
+% Start and End Points
+t0  = 0;
+tf  = 36;
+
+% Solve DDE using ddensd
+sol = ddensd(@ddefun_circ, dely, delyp, @history_circ, [t0  tf]);
+
+plotX   = [y_hist_need, sol.x];
+plotY   = [hist, sol.y];
+
+% Plotting
+figure(1);
+clf;
+plot(plotX, plotY);
+xlabel("Time t");
+ylabel("y(t)");
+legend("y_1(t)", "y_2(t)", "y_3(t)");
+% title("Tau = " + delay);
